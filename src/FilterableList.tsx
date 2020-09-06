e57c03adeec98d96edc61eb76f944369aa9384f1
jtlemkin <jtlemkin@gmail.com>
2020-09-06 14:07:36 -0700

    added ability to select counties from a filterable list

---
+++ src/FilterableList.tsx
@@ -0,0 +1,85 @@
+import React, { useState } from 'react'
+import PropTypes, { InferProps } from "prop-types"
+import styled from 'styled-components'
+import useWindowSize from './useWindowSize'
+
+const FixedDiv = styled.div`
+    flex: 0;
+`
+
+const Scrollable = styled.div`
+    height: 100%;
+    width: 200px;
+    overflow-y: scroll;
+`
+
+function SearchForm({
+    field, 
+    handleFieldChange
+}: InferProps<typeof SearchForm.propTypes>) {
+    const onChange = (event: any) => {
+        if (handleFieldChange !== null && handleFieldChange !== undefined) {
+            handleFieldChange(event)
+        }
+    }
+
+    return (
+        <form>
+            <label>
+                County:
+                <input type="text" value={field} onChange={onChange} />
+            </label>
+        </form>
+    )
+}
+
+SearchForm.propTypes = {
+    field: PropTypes.string.isRequired,
+    handleFieldChange: PropTypes.func,
+}
+
+export function FilterableList({ data, onClick }: FilterableListProps) {
+    const windowSize = useWindowSize()
+
+    const [field, setField] = useState('')
+    const handleFieldChange = (event: any) => {
+        setField(event.target.value)
+    }
+
+    const filteredData = data.filter(item => item.label.toLowerCase().startsWith(field.toLowerCase()))
+
+    return (
+        <FixedDiv style={{height: windowSize.height}}>
+            <SearchForm field={field} handleFieldChange={handleFieldChange} />
+            <Scrollable>
+                <ul>
+                    {filteredData.map((item, index) => {
+                        return (
+                            <li key={item.id}>
+                                <button onClick={() => { onClick(item) }}>
+                                    {item.label}
+                                </button>
+                            </li>
+                        )
+                    })}
+                </ul>
+            </Scrollable>
+        </FixedDiv>
+    )
+}
+
+interface FilterableListProps {
+    data: Array<DataPoint>,
+    onClick: (item: DataPoint) => void
+}
+
+interface DataPoint {
+    label: string,
+    id: string,
+}
+
+FilterableList.propTypes = {
+    data: PropTypes.arrayOf(
+        PropTypes.exact({label: PropTypes.string.isRequired, id: PropTypes.string.isRequired}).isRequired
+    ).isRequired,
+}
--- src/Map.tsx
+++ src/Map.tsx
@@ -1,9 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
+import styled from 'styled-components';
 import * as d3 from 'd3';
 import * as topojson from 'topojson-client'
 import { Topology, GeometryObject } from 'topojson-specification'
 import usUntyped from './counties-albers-10m.json'
 import useWindowSize from './useWindowSize'
+import { FilterableList } from './FilterableList'
+
+const Row = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-item: stretch;
+`
+
+const ExpandingDiv = styled.div`
+    flex: 1;
+`
 
 export function Map() {
     let containerRef: SVGSVGElement;
@@ -20,18 +32,50 @@ export function Map() {
 
     const windowSize = useWindowSize()
 
+    console.log(us)
+
+    const counties = usUntyped.objects.counties.geometries
+    const searchData = counties.map(county => {
+        return {
+            label: county.properties.name,
+            id: county.id.toString()
+        }
+    })
+
+    const [selectedCountyID, setSelectedCountyID] = useState<string | null>(null)
+
+    const selectCounty = (county: DataPoint) => {
+        setSelectedCountyID(county.id)
+    }
+
     return (
-        <div style={{maxWidth: windowSize.height * 975 / 610}}>
-            <svg
-            className="container"
-            ref={(ref: SVGSVGElement) => containerRef = ref}
-            viewBox={`0 0 975 610`}>
-                <g fill="none" stroke="#000" strokeLinejoin="round" strokeLinecap="round">
-                    <path stroke="#aaa" strokeWidth={0.5} d={countiesPath} ></path>
-                    <path strokeWidth={0.5} d={statesPath} ></path>
-                    <path d={nationPath}></path>
-                </g>
-            </svg>
-        </div>
+        <Row>
+            <FilterableList data={searchData} onClick={selectCounty} />
+            <ExpandingDiv style={{maxWidth: windowSize.height * 975 / 610}}>
+                <svg
+                className="container"
+                ref={(ref: SVGSVGElement) => containerRef = ref}
+                viewBox={`0 0 975 610`}>
+                    <g fill="none" stroke="#000" strokeLinejoin="round" strokeLinecap="round">
+                        {counties.map(countyGeometry => {
+                            const countyPath = path(topojson.feature(us, countyGeometry as GeometryObject)) ?? ""
+                            const color = selectedCountyID !== null && selectedCountyID === countyGeometry.id ? (
+                                'red'
+                            ) : (
+                                undefined
+                            )
+                            return <path stroke="#aaa" strokeWidth={0.5} fill={color} d={countyPath} key={countyGeometry.id}></path>
+                        })}
+                        <path strokeWidth={0.5} d={statesPath} ></path>
+                        <path d={nationPath}></path>
+                    </g>
+                </svg>
+            </ExpandingDiv>
+        </Row>
     );
+}
+
+interface DataPoint {
+    label: string,
+    id: string,
 }
