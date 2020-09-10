9dd61d22623eba94540be9f2dbc79a69abd15bb7
jtlemkin <jtlemkin@gmail.com>
2020-09-10 13:40:32 -0700

    added some boxes for where graphs will eventually go

--- src/Map.tsx
+++ src/Map.tsx
@@ -5,6 +5,7 @@ import useCanvas from './useCanvas'
 import { getRenderer } from './mapRenderer'
 import useWindowSize from './useWindowSize'
 import places from './places.json'
+import * as d3 from 'd3'
 
 const Row = styled.div`
   display: flex;
@@ -14,6 +15,15 @@ const Row = styled.div`
 
 const AsLargeAsPossibleDiv = styled.div`
     flex: 1;
+`
+
+const OutlinedBox = styled.div`
+    width: 250px;
+    height: 250px;
+    border-style: solid;
+    border-width: 2px;
+    border-color: 'black';
+    margin: 10px;
 `
 
 // A fips number is an identifier for counties, states, and the nation
@@ -39,6 +49,12 @@ export function Map() {
             <AsLargeAsPossibleDiv style={{maxWidth: maxMapWidth}}>
                 <canvas ref={canvasRef} width={width} height={height} style={{width, maxWidth: '100%'}}></canvas>
             </AsLargeAsPossibleDiv>
+            <div>
+                <p>Number of Cases Over Time</p>
+                <OutlinedBox />
+                <p>Number of Deaths Over Time</p>
+                <OutlinedBox />
+            </div>
         </Row>
     )
 }
