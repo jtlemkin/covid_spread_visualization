a4a4c654300d90d52e397c0c570dae08ef75e6d6
jtlemkin <jtlemkin@gmail.com>
2020-09-10 13:45:51 -0700

    Added title

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import './App.css'
 import { Map } from './Map'
+import styled from 'styled-components';
+
+const Title = styled.h1`
+`
 
 function App() {
   return (
     <div className="App">
+      <Title>Covid-19 Social Distancing Effectiveness</Title>
       <Map />
     </div>
   )
