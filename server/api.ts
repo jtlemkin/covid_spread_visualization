98e4f918e4caa2588f8180e5837f5a70b0500260
jtlemkin <jtlemkin@gmail.com>
2020-10-19 15:10:53 -0700

    added ability to search cities, counties, and states from server

--- server/api.ts
+++ server/api.ts
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
+import resultsFor from './getSearchResults'
+
 /* GET home page. */
 router.get('/', function(req: any, res: any, next: any) {
   res.send('Hello, World!');
@@ -9,6 +11,10 @@ router.get('/', function(req: any, res: any, next: any) {
 /* GET timeline */
 router.get('/timeline', function(req: any, res: any, next: any) {
   res.sendFile(__dirname + "/timeline.json");
+})
+
+router.get('/search/:str', function(req: any, res: any, next: any) {
+  res.json(JSON.stringify(resultsFor(req.params.str)))
 })
 
 module.exports = router;
